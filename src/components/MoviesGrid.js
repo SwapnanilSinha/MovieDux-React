31818d6e265cf5aa915a2d31d462294c2cd72667
Swapnanil Sinha <djkrunoze@gmail.com>
2025-03-29 20:34:43 +0530

    Added new features
    Searching, filtering and rating colors

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -4,17 +4,105 @@ import MovieCard from "./MovieCard";
 
 export default function MoviesGrid() {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [genre, setGenre] = useState("All Genres");
+  const [rating, setRating] = useState("All");
+
   useEffect(() => {
     fetch("movies.json")
       .then((response) => response.json())
       .then((data) => setMovies(data));
   }, []);
 
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleGenreChange = (event) => {
+    setGenre(event.target.value);
+  };
+  const handleRatingChange = (event) => {
+    setRating(event.target.value);
+  };
+
+  const movieMatchesGenre = (movie, genre) => {
+    return (
+      genre === "All Genres" ||
+      movie.genre.toLowerCase() === genre.toLowerCase()
+    );
+  };
+
+  const movieMatchesSearchTerm = (movie, searchTerm) => {
+    return movie.title.toLowerCase().includes(searchTerm.toLowerCase());
+  };
+
+  const movieMatchesRating = (movie, rating) => {
+    switch (rating) {
+      case "All Ratings":
+        return true;
+      case "Good":
+        return movie.rating >= 7;
+      case "Ok":
+        return movie.rating >= 5 && movie.rating < 7;
+      case "Bad":
+        return movie.rating < 5;
+      default:
+        return false;
+    }
+  };
+
+  const filteredMovies = movies.filter(
+    (movie) =>
+      movieMatchesGenre(movie, genre) &&
+      movieMatchesSearchTerm(movie, searchTerm) &&
+      movieMatchesRating(movie, rating)
+  );
+
   return (
-    <div className="movies-grid">
-      {movies.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} />
-      ))}
+    <div>
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Search for movies..."
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+
+      <div className="filter-bar">
+        <div className="filter-slot">
+          <label>Genre</label>
+          <select
+            className="filter-dropdown"
+            value={genre}
+            onChange={handleGenreChange}
+          >
+            <option value="All Genres">All Genres</option>
+            <option value="Action">Action</option>
+            <option value="Drama">Drama</option>
+            <option value="Fantasy">Fantasy</option>
+            <option value="Horror">Horror</option>
+          </select>
+        </div>
+
+        <div className="filter-slot">
+          <label>Rating</label>
+          <select
+            className="filter-dropdown"
+            value={rating}
+            onChange={handleRatingChange}
+          >
+            <option value="All Ratings">All Ratings</option>
+            <option value="Good">Good</option>
+            <option value="Ok">Ok</option>
+            <option value="Bad">Bad</option>
+          </select>
+        </div>
+      </div>
+      <div className="movies-grid">
+        {filteredMovies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
     </div>
   );
 }
